662ecb098d3bcb694173b6b0a6e95cc77a16ca60
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-03 09:43:39 -0600

    Add index to author_id foreign key from comments table

--- db/migrate/20230503150206_add_user_id_to_comments.rb
+++ db/migrate/20230503150206_add_user_id_to_comments.rb
@@ -1,5 +1,6 @@
 class AddUserIdToComments < ActiveRecord::Migration[7.0]
   def change
     add_reference :comments, :users, column: :author_id, null: false, foreign_key: true
+    add_index :comments, :users
   end
 end
