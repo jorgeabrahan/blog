2cc1c528e58739d63f21f5cce6f0f8e7ab09c314
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-04 14:03:55 -0600

    Update likes counter of post after save

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,10 @@
 class Like < ApplicationRecord
   belongs_to :user
-  belongs_to :post
+  belongs_to :post, counter_cache: true
+
+  private
+  # update likes counter of post after save
+  after_save do |like|
+    like.post.update(likes_counter: like.post.likes.count)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -7,8 +7,4 @@ class Post < ApplicationRecord
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
-
-  def update_likes_counter
-    update(likes_counter: likes.count)
-  end
 end
