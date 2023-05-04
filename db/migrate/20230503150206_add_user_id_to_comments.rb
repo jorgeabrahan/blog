ec0599fa37769577ad55bbfc082ae2edc2bb3d74
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-04 01:43:07 -0600

    Create tables foreign keys as references without indexes

--- db/migrate/20230503150206_add_user_id_to_comments.rb
+++
@@ -1,6 +0,0 @@
-class AddUserIdToComments < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :comments, :users, column: :author_id, null: false, foreign_key: true
-    add_index :comments, :users
-  end
-end
