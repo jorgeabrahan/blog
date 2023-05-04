5a73896ffb9bad40f98768b370b3812401cda927
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-04 10:24:03 -0600

    Fix rubocop linter errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
-  has_many :comments, class_name: "Comment"
-  has_many :likes, class_name: "Like"
-  
+  has_many :comments, class_name: 'Comment'
+  has_many :likes, class_name: 'Like'
+
   belongs_to :users
 
   def recent_comments
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :comments, class_name: "Comment"
-  has_many :posts, class_name: "Post"
-  has_many :likes, class_name: "Like"
+  has_many :comments, class_name: 'Comment'
+  has_many :posts, class_name: 'Post'
+  has_many :likes, class_name: 'Like'
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
