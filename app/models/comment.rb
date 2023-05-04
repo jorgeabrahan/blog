646a443f1006fa0a5ede246b3c307dcbe830f1fa
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-04 14:47:42 -0600

    Fix rubocop linter errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post, counter_cache: true
 
-
   # update comments counter of post after save
   after_save do |comment|
     comment.post.update(comments_counter: comment.post.comments.count)
