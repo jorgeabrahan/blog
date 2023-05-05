37dd603f18c68d8301b6243669a62e4003b874d0
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-05 10:45:02 -0600

    Create comments validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,9 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post, counter_cache: true
 
+  validates :text, presence: true
+  validates :text, length { maximum: 500 }
+  
   # update comments counter of post after save
   after_save do |comment|
     comment.post.update(comments_counter: comment.post.comments.count)
