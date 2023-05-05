df11927e8332fcbc97a670edecde1c7d8ad863d0
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-05 07:57:16 -0600

    Validate name in user model to not be blank

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :posts, class_name: 'Post'
   has_many :likes, class_name: 'Like'
 
+  validates :name, presence: true
+
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
