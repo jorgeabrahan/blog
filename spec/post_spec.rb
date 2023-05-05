07b59878522c61a6ef477cc55e83e0dfd5164239
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-05 11:51:46 -0600

    Validate the get most recent comments from each post method

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -19,4 +19,17 @@ RSpec.describe Post, type: :model do
   it 'Should not be valid if likes counter is less than 0' do
     expect(Post.new(likes_counter: -20)).to_not be_valid
   end
+  it 'returns the 5 most recent comments sorted by created_at' do
+    post = Post.create(title: 'My Post')
+    comment1 = Comment.create(text: 'Comment 1', created_at: 1.day.ago)
+    comment2 = Comment.create(text: 'Comment 2', created_at: 2.days.ago)
+    comment3 = Comment.create(text: 'Comment 3', created_at: 3.days.ago)
+    comment4 = Comment.create(text: 'Comment 4', created_at: 4.days.ago)
+    comment5 = Comment.create(text: 'Comment 5', created_at: 5.days.ago)
+    comment6 = Comment.create(text: 'Comment 6', created_at: 6.days.ago)
+
+    post.comments << [comment1, comment2, comment3, comment4, comment5, comment6]
+    
+    expect(post.recent_comments).to eq([comment1, comment2, comment3, comment4, comment5])
+  end
 end
