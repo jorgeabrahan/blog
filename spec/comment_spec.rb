18597e6401fe684f774ccc63de5bbf6afc947dfb
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-05 19:23:06 -0600

    Require the models to test in the spec_helper file

--- spec/comment_spec.rb
+++ spec/comment_spec.rb
@@ -8,6 +8,9 @@ RSpec.describe Comment, type: :model do
     expect(Comment.new(text: '')).to_not be_valid
   end
   it 'Should have a text with a max length of 500 characters' do
-    expect(Comment.new(text: 'This comment should be valid')).to be_valid
+    user = User.create!(name: "John", photo: 'https://photo.png', posts_counter: 1)
+    post = Post.create!(user_id: user.id, title: "Hello, world!", comments_counter: 1, likes_counter: 0)
+    comment = Comment.create(user_id: user.id, post_id: post.id, author: user, post: post, text: "Great post!")
+    expect(comment).to be_valid
   end
 end
