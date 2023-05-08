79b048c206f68af8eeb027330a321322b4f1489f
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-08 16:15:09 -0600

    Fix rubocop linter errors

--- spec/requests/users/posts_spec.rb
+++ spec/requests/users/posts_spec.rb
@@ -1,18 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe "Users::Posts", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/users/posts/index"
+RSpec.describe 'Users::Posts', type: :request do
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/users/posts/index'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /show" do
-    it "returns http success" do
-      get "/users/posts/show"
+  describe 'GET /show' do
+    it 'returns http success' do
+      get '/users/posts/show'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
