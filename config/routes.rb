11c8d9b090edaf46ad867892f96aca6b04904d9e
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-08 16:38:34 -0600

    Refactor routes configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :show]
-
-  resources :users do
+  resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
