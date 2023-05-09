0512140d45da7608327a0ef826c084900f316107
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-09 12:49:03 -0600

    Create tests to check if a correct template was rendered

--- spec/requests/users/posts_spec.rb
+++ spec/requests/users/posts_spec.rb
@@ -6,12 +6,22 @@ RSpec.describe Users::PostsController, type: :controller do
       get :index, params: { user_id: 1 }
       expect(response).to have_http_status(:success)
     end
+
+    it "renders the index template" do
+      get :index, params: { user_id: 1 }
+      expect(response).to render_template("users/posts/index")
+    end
   end
 
   describe "GET /users/:user_id/posts/:id" do
     it "returns http success" do
       get :show, params: { user_id: 1, id: 1 }
       expect(response).to have_http_status(:success)
+    end
+
+    it "renders the show template" do
+      get :show, params: { user_id: 1, id: 1 }
+      expect(response).to render_template("users/posts/show")
     end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -6,12 +6,22 @@ RSpec.describe UsersController, type: :controller do
       get :index
       expect(response).to have_http_status(:success)
     end
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("users/index")
+    end
   end
 
   describe 'GET /users/:id' do
     it 'returns http success' do
       get :show, params: { id: 1 }
       expect(response).to have_http_status(:success)
+    end
+
+    it "renders the show template" do
+      get :show, params: { id: 1 }
+      expect(response).to render_template("users/show")
     end
   end
 end
