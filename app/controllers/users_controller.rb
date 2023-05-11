0e4105fa38e2c0d468df9dcecc803817b164efd2
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-11 02:28:27 -0600

    Get current user based on the id got from the url parameter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
-
-  def show; end
+  
+  def show
+    @user = User.find { |user| user.id == params[:id].to_i }
+  end
 end
