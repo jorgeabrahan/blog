c1e594235762a07ed374301a4567ef9cf7c4030d
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-12 10:53:43 -0600

    Fix rubocop linter errors

--- app/controllers/users/posts/likes_controller.rb
+++ app/controllers/users/posts/likes_controller.rb
@@ -3,10 +3,9 @@ class Users::Posts::LikesController < ApplicationController
     @like = Like.new
     @like.author = current_user
     @like.post = Post.find { |post| post.id == params[:post_id].to_i }
-    
-    if @like.save
-      redirect_to user_post_path(user_id: params[:user_id].to_i, id: params[:post_id].to_i)
-    end
 
+    return unless @like.save
+
+    redirect_to user_post_path(user_id: params[:user_id].to_i, id: params[:post_id].to_i)
   end
 end
