eba2aec0d3a67a4f6541f9ad41213cb4e67c9249
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-12 09:34:23 -0600

    Save likes in the database

--- app/controllers/users/posts/likes_controller.rb
+++ app/controllers/users/posts/likes_controller.rb
@@ -3,7 +3,10 @@ class Users::Posts::LikesController < ApplicationController
     @like = Like.new
     @like.author = current_user
     @like.post = Post.find { |post| post.id == params[:post_id].to_i }
+    
+    if @like.save
+      redirect_to user_post_path(user_id: params[:user_id].to_i, id: params[:post_id].to_i)
+    end
 
-    redirect_to user_post_path(user_id: params[:user_id].to_i, id: params[:post_id].to_i)
   end
 end
