17f1199d6c458a0cb5a62f6c822c33ca3fade087
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-11 19:15:38 -0600

    Create method for the new action that makes available a new post and the current user for the view

--- app/controllers/users/posts_controller.rb
+++ app/controllers/users/posts_controller.rb
@@ -7,4 +7,9 @@ class Users::PostsController < ApplicationController
     @user = User.find { |user| user.id == params[:user_id].to_i }
     @post = Post.find { |post| ((post.id == params[:id].to_i) && (post.author_id == params[:user_id].to_i)) }
   end
+
+  def new
+    @post = Post.new
+    @user = current_user
+  end
 end
