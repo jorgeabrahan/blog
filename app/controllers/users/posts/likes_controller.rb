6254feb0b7738871f2999b56b0bac1f1f147bdfe
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-12 02:21:39 -0600

    Create likes controller with create action to manage the form to like a post

---
+++ app/controllers/users/posts/likes_controller.rb
@@ -0,0 +1,13 @@
+class Users::Posts::LikesController < ApplicationController
+  def create
+    @like = Like.new
+    @like.author = current_user
+    @like.post = Post.find { |post| post.id == params[:post_id].to_i }
+
+    if @like.save
+      redirect_to user_post_path(user_id: params[:user_id].to_i, id: params[:post_id].to_i)
+    else
+      redirect_to user_post_path(user_id: params[:user_id].to_i, id: params[:post_id].to_i)
+    end
+  end
+end
