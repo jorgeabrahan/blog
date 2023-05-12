900b63d8d8e371aea678ed62a3fb45bb0790462b
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-12 07:55:33 -0600

    Remove new action from likes resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :posts, controller: 'users/posts' do
       resources :comments, only: [:new, :create], controller: 'users/posts/comments'
-      resources :likes, only: [:new, :create], controller: 'users/posts/likes'
+      resources :likes, only: [:create], controller: 'users/posts/likes'
     end
   end
 
