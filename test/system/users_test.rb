ba3d3d1d92748183a29559173ad887cc293b74e4
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-16 13:16:23 -0600

    Create test to check if the amount of posts of each user is being displayed

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -11,4 +11,14 @@ class UsersTest < ApplicationSystemTestCase
     visit users_path
     assert_selector "img[src='https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541']"
   end
+
+  test 'Number of posts each user has written is being displayed' do
+    visit users_path
+    within first('.user') do
+      assert_text 'Amount of posts: 2'
+    end
+    within all('.user')[1] do
+      assert_text 'Amount of posts: 3'
+    end
+  end
 end
