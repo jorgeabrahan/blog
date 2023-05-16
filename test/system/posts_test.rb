9cf13ec5b6414ee877f3a6f140c843abe7f3302c
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-16 16:34:36 -0600

    Create test to check if the user username is being displayed

--- test/system/posts_test.rb
+++ test/system/posts_test.rb
@@ -5,4 +5,8 @@ class PostsTest < ApplicationSystemTestCase
     visit user_posts_path(users(:one))
     assert_selector "img[src='https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541']"
   end
+  test 'User username is being displayed' do
+    visit user_posts_path(users(:one))
+    assert_text "Jorge"
+  end
 end
