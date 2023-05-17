3c9eb6f1fae16e9a8b7e2b90fdb3907e21dc60d7
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-05-17 13:23:11 -0400

    Add test case for user's username

--- test/system/user_test.rb
+++ test/system/user_test.rb
@@ -6,4 +6,9 @@ class UsersTest < ApplicationSystemTestCase
     visit user_path(users(:one))
     assert_selector "img[src='https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541']"
   end
+
+  test 'User username is being displayed' do
+    visit user_path(users(:one))
+    assert_text 'Jorge'
+  end
 end
