69ede32865444da2d03649a49174125be2d28c78
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-18 23:52:25 -0600

    Change dependent value for comments and likes from nullify to destroy to remove comments and likes for a given post when it is removed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
 
-  has_many :comments, dependent: :nullify, foreign_key: 'post_id'
-  has_many :likes, dependent: :nullify, foreign_key: 'post_id'
+  has_many :comments, dependent: :destroy, foreign_key: 'post_id'
+  has_many :likes, dependent: :destroy, foreign_key: 'post_id'
 
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
