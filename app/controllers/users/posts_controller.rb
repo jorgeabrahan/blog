7fba717cefd9e6d12efbe7764d294dda45d05ae9
jorgeabrahan <jorge24abrahan@gmail.com>
2023-10-10 03:10:14 -0600

    Create controllers helpers views and tests

---
+++ app/controllers/users/posts_controller.rb
@@ -0,0 +1,7 @@
+class Users::PostsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  namespace :users do
+    get 'posts/index'
+    get 'posts/show'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
